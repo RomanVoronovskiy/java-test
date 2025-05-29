f12862aef85636f70fdca2bd86b9c90d75a698a6
Roman Voronovskii <121174759+RomanVoronovskiy@users.noreply.github.com>
2025-05-29 21:42:45 +0300

    add realization

--- src/main/java/ru/otus/Main.java
+++ src/main/java/ru/otus/Main.java
@@ -4,25 +4,26 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.class.getName());
+    private static final Logger log = LogManager.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
 
         SimpleCalculator simpleCalculator = new SimpleCalculator();
         double result = 0;
-//        try {
-//            if (simpleCalculator.add(8, 4) == 13) {
-//                result = 12;
-//            } else {
-//                throw new AssertionError("Ошибка в методе");
-//            }
-//        } catch (AssertionError e) {
-//            throw new RuntimeException(e);
-//        }
-//        assert 6 == simpleCalculator.multiply(2, 3);
-        while (true) {
-            simpleCalculator.add(3, 5);
-            logger.error("все потоки мертвы сейчас прирвемся!");
+        try {
+            if (simpleCalculator.add(8, 4) == 13) {
+                result = 12;
+            } else {
+                log.warn("Получили нестыковку пытаемся обработать");
+                throw new AssertionError("Ошибка в методе");
+            }
+        } catch (AssertionError e) {
+            log.error("Падаем окончательно");
+            throw new RuntimeException(e);
         }
+//        assert 6 == simpleCalculator.multiply(2, 7) : "вот тут ошибка!";
+        simpleCalculator.add(3, 5);
+        log.info("все успешно");
+
     }
 }
